f41363d50e22e9129b74beb3221ee2a8d4bd93f4
Brian <brian.sooyong.shin@gmail.com>
2019-03-07 23:39:31 +0900

    google link for the writing class and instructions

--- resources/views/writing.blade.php
+++ resources/views/writing.blade.php
@@ -5,3 +5,18 @@
 @section('content')
     Writing Class
 @endsection
+
+@section('body')
+<br>
+<br>
+    <h3>Steps to submitting your homework:</h3>
+    1.  click on google drive link below.<br>
+    2.  find your class folder.<br>
+    3.  find your own folder (if not available, make one).<br>
+    4.  submit either a google doc or word document (HWP does not work).<br>
+
+    <div class="links">
+        <a target="_blank" href="https://drive.google.com/drive/folders/1iEL_IMo5UUz3lXiOp075_Ksq7RAAKG-n?usp=sharing">Google Link</a>
+    </div>
+    
+@endsection
