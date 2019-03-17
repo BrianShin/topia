324af4f92a36a4f71abc722f75ea8295ee13a7e6
Brian <brian.sooyong.shin@gmail.com>
2019-03-17 21:17:24 +0900

    cant upload laravel framework, therefore, uploaded a single page php.

---
+++ writing TOPIA/index.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Topia Writing</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                min-height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Writing Class
+                </div>
+                <h3>Before submitting:</h3>
+                1.  proofread your work.<br>
+                <div class="links">
+                    <a target="_blank" href="https://www.grammarly.com/">Grammarly Link</a>
+                </div>
+
+                <h3>Steps to submitting your homework: (if you have a gmail)</h3>
+                1.  click on google drive link below.<br>
+                2.  find your class/name folder.<br>
+                3.  submit either a google doc or word document (HWP does not work).<br>
+                <div class="links">
+                    <a target="_blank" href="https://drive.google.com/drive/folders/1uJb8fZMIC7x6rOyYD8jtbJa286YzmtEa?usp=sharing">Google Link</a>
+                </div>
+
+                <h3>Steps to submitting your homework: (if you DO NOT have a gmail)</h3>
+                1.  email a word document (not HWP or just email).<br>
+                2.  email: with.brianteacher@gmail.com<br>
+                3.  example- title: class name, your name<br>
+                <img src="emailexample.jpg" alt="email example" width="400"> 
+            </div>
+        </div>
+    </body>
+</html>
